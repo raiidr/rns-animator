24c25dfb12f787cb1300faf783f4ccc07229aa94
Invariant-tech-green <greengraphicx@invarianttechnologies.com>
2025-07-24 04:00:57 +0000

    Update

--- plugin/index.js
+++ plugin/index.js
@@ -1,26 +1,38 @@
-/// rns-animator\plugin\index.js
-const { withMainApplication } = require('@expo/config-plugins');
+// rns-animator\plugin\index.js
+const {
+    withMainApplication,
+    withAndroidManifest,
+    withProjectBuildGradle,
+} = require('@expo/config-plugins');
 
 module.exports = function withRNSAnimator(config) {
-    return withMainApplication(config, (mod) => {
-        // Inject import if not already present
-        if (!mod.contents.includes('import com.rnsanimator.www.LayoutAnimatorPackage')) {
-            mod.contents = mod.contents.replace(
-                /package .*?\n/,
-                match => match + 'import com.rnsanimator.www.LayoutAnimatorPackage;\n'
+    // 1. Inject the package import & registration
+    config = withMainApplication(config, async config => {
+        const contents = config.modResults.contents;
+
+        // 1a. Add import
+        const importStatement = `import com.rnsanimator.www.LayoutAnimatorPackage;`;
+        if (!contents.includes(importStatement)) {
+            config.modResults.contents = contents.replace(
+                /package\s+[\w.]+;\r?\n/,
+                `&\n${importStatement}`
             );
         }
 
-        // Inject package in getPackages()
-        if (!mod.contents.includes('new LayoutAnimatorPackage()')) {
-            mod.contents = mod.contents.replace(
-                /val packages = PackageList\(this\)\.packages([\s\S]*?)return packages;/,
-                (match, inner) => {
-                    return `val packages = PackageList(this).packages${inner}    packages.add(LayoutAnimatorPackage())\n    return packages;`;
-                }
-            );
-        }
+        // 1b. Register the package in getPackages()
+        config.modResults.contents = config.modResults.contents.replace(
+            /packages = PackageList\(this\)\.packages;/,
+            `packages = PackageList(this).packages;\n    packages.add(new LayoutAnimatorPackage());`
+        );
 
-        return mod;
+        return config;
     });
+
+    // 2. (Optional) Ensure your android/build.gradle has react-native plugin
+    config = withProjectBuildGradle(config, config => {
+        // You can check and inject `apply plugin: 'com.facebook.react'` etc.
+        return config;
+    });
+
+    return config;
 };
